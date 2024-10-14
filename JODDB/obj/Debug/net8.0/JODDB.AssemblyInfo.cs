89859e49ca430b078f582089f90159560c6611ff
Ad31 Sh <68187289+ad31sh@users.noreply.github.com>
2024-10-14 20:56:35 +0300

    home page done

--- JODDB/obj/Debug/net8.0/JODDB.AssemblyInfo.cs
+++ JODDB/obj/Debug/net8.0/JODDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JODDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed2d70e2da68d1b92528fc285c139d49456657f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea538b33a73e0c1721ab02485f72e9dadb94020")]
 [assembly: System.Reflection.AssemblyProductAttribute("JODDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JODDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
